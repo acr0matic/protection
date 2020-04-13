d1694b3671911ff5b139fa79d51d3879221be2a8
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-04-13 15:40:40 +0300

    Добавлен функционал для кнопок и добавлены некоторые товары

--- src/scripts/compiled/script.js
+++ src/scripts/compiled/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+var callbackButton = document.getElementById("callback-button");
+var headerCart = document.getElementById("cart-header");
+callbackButton.addEventListener("click", function () {
+  MicroModal.show("modal-callback");
+});
+headerCart.addEventListener("mouseenter", function () {
+  MicroModal.show("modal-cart");
+});
+headerCart.addEventListener("mouseleave", function () {
+  MicroModal.close("modal-cart");
+});
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,25 @@
 var callbackButton = document.getElementById("callback-button");
 
 var headerCart = document.getElementById("cart-header");
+var checkoutButton = document.getElementById("checkout-button");
+var closeCart = document.getElementById("close-cart");
+
+var headerButtonCatalog = document.querySelector("#hero .button-primary")
+var headerButtonAbout = document.querySelector("#hero .button-clear")
+
+headerButtonCatalog.addEventListener("click", () => {
+  document.getElementById("production").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  })
+})
+
+headerButtonAbout.addEventListener("click", () => {
+  document.getElementById("order-info").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  })
+})
 
 callbackButton.addEventListener("click", () => {
   MicroModal.show("modal-callback");
@@ -14,5 +33,10 @@ headerCart.addEventListener("mouseleave", () => {
   MicroModal.close("modal-cart");
 });
 
-MicroModal.show("modal-cart-full");
+checkoutButton.addEventListener("click", () => {
+  MicroModal.show("modal-cart-full");
+});
 
+closeCart.addEventListener("click", () => {
+  MicroModal.close("modal-cart-full");
+});
