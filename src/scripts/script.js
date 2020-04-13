0bc8c885fa3561951497a13bd06ae26af969cb0a
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-04-13 03:30:18 +0300

    Сверстана пустая корзина

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,17 @@
 var callbackButton = document.getElementById("callback-button");
 
+var headerCart = document.getElementById("cart-header");
+
 callbackButton.addEventListener("click", () => {
   MicroModal.show("modal-callback");
 });
 
+headerCart.addEventListener("mouseenter", () => {
+  MicroModal.show("modal-cart-empty");
+})
+
+headerCart.addEventListener("mouseleave", () => {
+  MicroModal.close("modal-cart-empty");
+})
 
 
